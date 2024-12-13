4d3d576a9c98f6485012ca61245e8ea15e4adf03
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-12-13 17:51:41 +0200

    deploy commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Disable cache in development for quicker testing and reloading
+    watch: {
+      usePolling: true, // Forces Vite to disable caching in some cases
+    },
+    headers: {
+      'Cache-Control': 'no-store', // Prevents caching of files in the browser during development
+    },
+  },
+  build: {
+    // Enable versioning of assets in production
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+});
