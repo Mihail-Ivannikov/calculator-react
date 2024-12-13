89118cd24f46938f811d32423b344807ec7a2cd5
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-12-13 18:11:05 +0200

    fix2

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/calculator-react/',
+  base: '/calculator-react/', // Ensure this matches your GitHub Pages repository
   plugins: [react()],
   server: {
-    // Disable cache in development for quicker testing and reloading
     watch: {
-      usePolling: true, // Forces Vite to disable caching in some cases
+      usePolling: true, // Ensure Vite watches changes correctly
     },
     headers: {
-      'Cache-Control': 'no-store', // Prevents caching of files in the browser during development
+      'Cache-Control': 'no-store', // Disable caching during development
     },
   },
   build: {
-    // Enable versioning of assets in production
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
